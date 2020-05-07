011fe6f7f1c751ec197adf900eb718c42cdf1b95
feuwie <56365722+feuwie@users.noreply.github.com>
2020-05-07 23:06:30 +0300

    sdfsdf

--- src/main/java/com/simakov/diploma/Controllers/ProfileController.java
+++ src/main/java/com/simakov/diploma/Controllers/ProfileController.java
@@ -63,6 +63,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -73,6 +74,7 @@ import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.ReplaySubject;
 import io.reactivex.subjects.Subject;
 
+@RequestMapping("/")
 @CrossOrigin(origins = "*")
 @RestController
 public class ProfileController {
--- src/main/java/com/simakov/diploma/DemoApplication.java
+++ src/main/java/com/simakov/diploma/DemoApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
