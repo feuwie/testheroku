94a7ac4567dde66ecca0cecb3fbe779bda29f973
feuwie <56365722+feuwie@users.noreply.github.com>
2020-05-07 23:16:06 +0300

    sdfsdfd

--- src/main/java/com/simakov/diploma/DemoApplication.java
+++ src/main/java/com/simakov/diploma/DemoApplication.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/api")
+
 @SpringBootApplication
 public class DemoApplication {
 
